a9fd6b99e55ccfa6461f716a388d855e22471162
kubik20083 <kubik20083@mail.ru>
2024-04-09 21:31:17 +0300

    task1

--- task1.c
+++ task1.c
@@ -1,7 +1,23 @@
+// Задача 1. В прямом порядке
+
 #include<stdio.h>
+
+void printNum (int);
+
 
 int main(int argc, char const *argv[])
 {
+    int a;
+    scanf("%d", &a);
+    printNum(a);
     
     return 0;
+}
+
+
+void printNum (int n)
+{
+    if (n >= 10)
+        printNum(n / 10);
+    printf("%d\n", n % 10);
 }
